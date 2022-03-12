5cf9f4e0b4547b2c6b9440cdef6c25cb32273c5d
Danishjeet Singh <danishjeetsingh@Danishjeets-MBP.hsd1.in.comcast.net>
2022-03-12 18:51:40 -0500

    breadthFirstSearch.js added

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -4,23 +4,56 @@ function addTo80(n){
     return n + 80;
 }
 
-// cache is an empty object, which is supposed to store key value pairs,
-// and it will act as an actual cache 
-
-let cache = {};
-
-
 // memoizedAddTo80 function uses cache to work
 
-function memoizedAddTo80(n){
-    if (n in cache){
-        return cache[n];
-    } else {
-        console.log("executing else block")
-        cache[n] = 5 + 80;
-        return cache[n];
+function memoizedAddTo80(){
+    let cache = {}; // cache is an empty object, which is supposed to store key value pairs, and it will act as an actual cache 
+    return function(n){
+        if (n in cache){
+            return cache[n];
+        } else {
+            console.log("executing else block");
+            cache[n] = n + 80;
+            return cache[n];
+        }
     }
 }
 
-console.log(1, memoizedAddTo80(5));
-console.log(2, memoizedAddTo80(5));
+const memoized = memoizedAddTo80();
+
+console.log(1, memoized(5));
+console.log(2, memoized(5));
+console.log(3, memoized(2));
+console.log(4, memoized(2));
+
+
+// fibonacci sequence with memoization 
+
+function fibonacciMaster(){ // top down approach
+    let cache = {};
+    return function fib(n){
+        if(n in cache){
+            return cache[n];
+        }else{
+            if(n < 2){
+                return n;
+            } else{
+                cache[n] = fib(n - 1) + fib(n - 2);
+                return cache[n];
+            }
+        }
+    }
+}
+
+function fibonacciMaster2(n){ // bottom up approach
+    let answer = [0, 1];
+    for(let i = 2; i <= n; i++){
+        answer.push(answer[i - 1] + answer[i - 2]);
+    }
+    return answer.pop();
+}
+
+const fasterFib = fibonacciMaster();
+
+console.log(fasterFib(12))
+console.log(fibonacciMaster2(12))
